1f924c9705db3f0eb6784e677070747b73d95a3d
shovonchowdhury <debojjoti550@gmail.com>
2024-10-21 02:08:02 +0600

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.html"],
+  darkMode: 'selector',
   theme: {
     extend: {
       transitionTimingFunction: {
         jump: "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
       },
     },
+   
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
